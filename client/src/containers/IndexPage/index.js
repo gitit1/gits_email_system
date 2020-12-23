6cb20c82cb5e2834a20c64cb1d8519b6762aef27
Gitit <gitit1@msn.com>
2020-12-23 21:40:14 +0200

    designed index page

--- client/src/containers/IndexPage/index.js
+++ client/src/containers/IndexPage/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './indexPage.scss';
+import DraftsIcon from '@material-ui/icons/Drafts';
 
 const IndexPage = props => {
 
   return (
-    <h1>This is the Index page</h1>
+    <div className="index-page">
+      <DraftsIcon className="index-page__icon" />
+      <div className="index-page__text">
+        <h1>Hello and Welcome to Git's Email System!</h1>
+        <h3>Please Registrer/Login!</h3>
+      </div>
+    </div>
   );
 };
 
